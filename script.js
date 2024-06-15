a7e0e53780a295290372bb7c91854f3ed66007af
Al-Faris Mujahid AlZakwani <107947850+faris-zak@users.noreply.github.com>
2024-06-16 01:07:03 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function myFunction(){
+    alert("Try to know us (;");
+    for(var i = 0; true; i++){
+        console.log(i);
+    }
+}
+
+function testFunction(){
+    alert("Everything works properly!");
+}
